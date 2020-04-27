3ec314ea9c192a222337545c8b7d64f3d743a06b
Jody Mickey <jody.mickey@gmail.com>
2020-04-27 17:42:27 -0400

    use secure urls for imgur

--- src/js/images.js
+++ src/js/images.js
@@ -1,6 +1,6 @@
 export default [
-    'http://i.imgur.com/Udz7RRD.jpg',
-    'http://i.imgur.com/OG4Filt.jpg',
-    'http://i.imgur.com/Dm3yaqR.jpg',
-    'http://i.imgur.com/qkUnoDt.jpg'
+    'https://i.imgur.com/Udz7RRD.jpg',
+    'https://i.imgur.com/OG4Filt.jpg',
+    'https://i.imgur.com/Dm3yaqR.jpg',
+    'https://i.imgur.com/qkUnoDt.jpg'
 ];
